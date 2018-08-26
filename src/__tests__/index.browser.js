6ec722af4788ed00213da8a9eb71cc0f37961213
Leath Cooper <IcculusC@users.noreply.github.com>
2018-08-26 01:22:03 -0400

    test suite (#1)

--- src/__tests__/index.browser.js
+++ src/__tests__/index.browser.js
@@ -2,26 +2,76 @@
 /* eslint-env browser */
 import tape from 'tape-cup';
 import React from 'react';
-import Enzyme from 'enzyme';
+import Enzyme, {mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import App from 'fusion-core';
 import {getService} from 'fusion-test-utils';
+import {create as createJss} from 'jss';
+
+import JssProvider from 'react-jss/lib/JssProvider';
+import {MuiThemeProvider} from '@material-ui/core/styles';
 
 import Plugin from '../browser.js';
+import {JssToken, MuiThemeToken} from '../tokens';
 
 Enzyme.configure({adapter: new Adapter()});
 
 /* Test fixtures */
-const appCreator = () => {
+const testJss = createJss();
+const testTheme = {foo: 'bar'};
+
+const appCreator = (theme, jss) => {
   const app = new App('test', el => el);
+  if (theme) {
+    app.register(MuiThemeToken, theme);
+  }
+  if (jss) {
+    app.register(JssToken, jss);
+  }
   return () => app;
 };
 
-tape('browser middleware', async t => {
+tape('provides a custom jss instance', async t => {
+  const element = React.createElement('div');
+  const ctx: any = {element, template: {body: []}, memoized: new Map()};
+  const service = getService(appCreator(null, testJss), Plugin);
+
+  t.plan(1);
+
+  t.equals(service.from(ctx).jss, testJss, 'passes along the jss instance');
+
+  t.end();
+});
+
+tape('provides theme', async t => {
+  const element = React.createElement('div');
+  const ctx: any = {element, template: {body: []}, memoized: new Map()};
+  const serviceWithTheme = getService(appCreator(testTheme), Plugin);
+  const serviceWithoutTheme = getService(appCreator(), Plugin);
+
+  t.plan(2);
+
+  t.equals(serviceWithTheme.from(ctx).theme.foo, 'bar', 'passes along theme');
+  t.notEqual(
+    serviceWithoutTheme.from(ctx).theme,
+    null,
+    'but has a default theme'
+  );
+
+  t.end();
+});
+
+tape('removes useless SSR styles after render', async t => {
+  const ssrStyles = document.createElement('style');
+  ssrStyles.setAttribute('type', 'text/css');
+  ssrStyles.setAttribute('id', '__MUI_STYLES__');
+  document.body.appendChild(ssrStyles);
   const element = React.createElement('div');
   const ctx: any = {element, template: {body: []}, memoized: new Map()};
   const service = getService(appCreator(), Plugin);
+  t.plan(2);
+  t.notEquals(document.getElementById('__MUI_STYLES__'), null);
   try {
     await (Plugin.middleware &&
       // $FlowFixMe
@@ -29,6 +79,61 @@ tape('browser middleware', async t => {
   } catch (e) {
     t.ifError(e);
   }
+  t.equals(document.getElementById('__MUI_STYLES__'), null);
+  t.end();
+});
 
+tape('browser middleware with default theme', async t => {
+  const element = React.createElement('div');
+  const ctx: any = {element, template: {body: []}, memoized: new Map()};
+  const service = getService(appCreator(testTheme), Plugin);
+  try {
+    await (Plugin.middleware &&
+      // $FlowFixMe
+      Plugin.middleware(null, service)((ctx: any), () => Promise.resolve()));
+  } catch (e) {
+    t.ifError(e);
+  }
+  t.plan(2);
+  const rendered = mount(ctx.element);
+  t.equal(rendered.find(MuiThemeProvider).length, 1);
+  t.notEquals(rendered.find(MuiThemeProvider).props().theme, null);
+  t.end();
+});
+
+tape('browser middleware with registered theme', async t => {
+  const element = React.createElement('div');
+  const ctx: any = {element, template: {body: []}, memoized: new Map()};
+  const service = getService(appCreator(testTheme), Plugin);
+  try {
+    await (Plugin.middleware &&
+      // $FlowFixMe
+      Plugin.middleware(null, service)((ctx: any), () => Promise.resolve()));
+  } catch (e) {
+    t.ifError(e);
+  }
+  t.plan(2);
+  const rendered = mount(ctx.element);
+  t.equal(rendered.find(MuiThemeProvider).length, 1);
+  t.equal(rendered.find(MuiThemeProvider).props().theme, testTheme);
+  t.end();
+});
+
+tape('browser middleware with jss', async t => {
+  const element = React.createElement('div');
+  const ctx: any = {element, template: {body: []}, memoized: new Map()};
+  const testJss = createJss();
+  const service = getService(appCreator(null, testJss), Plugin);
+  try {
+    await (Plugin.middleware &&
+      // $FlowFixMe
+      Plugin.middleware(null, service)((ctx: any), () => Promise.resolve()));
+  } catch (e) {
+    t.ifError(e);
+  }
+  t.plan(2);
+  const rendered = mount(ctx.element);
+  t.equal(rendered.find(JssProvider).length, 1);
+  t.equal(rendered.find(JssProvider).props().jss, testJss);
   t.end();
 });
--- src/__tests__/index.node.js
+++ src/__tests__/index.node.js
@@ -4,32 +4,67 @@ import React from 'react';
 
 import App, {consumeSanitizedHTML} from 'fusion-core';
 import {getService} from 'fusion-test-utils';
+import {create as createJss} from 'jss';
 
-import Plugin from '../index.js';
+import Plugin, {JssToken, MuiThemeToken} from '../index.js';
 
 /* Test fixtures */
-const appCreator = () => {
+const appCreator = (theme, jss) => {
   const app = new App('test', el => el);
+  if (theme) {
+    app.register(MuiThemeToken, theme);
+  }
+  if (jss) {
+    app.register(JssToken, jss);
+  }
   return () => app;
 };
 
-tape('serialization', async t => {
+tape('provides a custom jss instance', async t => {
   const element = React.createElement('div');
+  const ctx: any = {element, template: {body: []}, memoized: new Map()};
+  const testJss = createJss();
+  const service = getService(appCreator(null, testJss), Plugin);
+
+  t.plan(1);
+
+  t.equals(service.from(ctx).jss, testJss, 'passes along the jss instance');
+
+  t.end();
+})
+
+tape('provides theme', async t => {
+  const element = React.createElement('div');
+  const ctx: any = {element, template: {body: []}, memoized: new Map()};
+  const serviceWithTheme = getService(appCreator({foo: 'bar'}), Plugin);
+  const serviceWithoutTheme = getService(appCreator(), Plugin);
+
+  t.plan(2);
+
+  t.equals(serviceWithTheme.from(ctx).theme.foo, 'bar', 'passes along theme');
+  t.notEqual(
+    serviceWithoutTheme.from(ctx).theme.spacing.unit,
+    null,
+    'but has a default theme'
+  );
+
+  t.end();
+});
+
+tape('serialization', async t => {
+  const element = React.createElement('div'); // Button, {variant: 'contained', color: 'primary'});
   const ctx: any = {element, template: {body: []}, memoized: new Map()};
   const service = getService(appCreator(), Plugin);
 
-  t.plan(5);
-  if (!Plugin.middleware) {
-    t.end();
-    return;
-  }
+  t.plan(3);
 
   // $FlowFixMe
   await Plugin.middleware(null, service)(ctx, () => Promise.resolve());
-
   t.equals(ctx.template.body.length, 1, 'pushes serialization to body');
-  // $FlowFixMe
-  t.equals(consumeSanitizedHTML(ctx.template.body[0]).match('test')[0], 'test');
+  t.equals(
+    consumeSanitizedHTML(ctx.template.body[0]).match('</style>').input,
+    '<style type="text/css" id="__MUI_STYLES__"></style>'
+  );
   t.equals(consumeSanitizedHTML(ctx.template.body[0]).match('</div>'), null);
   t.end();
 });
