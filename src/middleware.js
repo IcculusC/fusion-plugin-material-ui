a3c4877a084aef8e106b4f6b084ea744cc9674c0
Leath Cooper <IcculusC@users.noreply.github.com>
2018-08-30 01:04:41 -0400

    fixing some hydration issues (#8)

--- src/middleware.js
+++ src/middleware.js
@@ -5,13 +5,19 @@ import JssProvider from 'react-jss/lib/JssProvider';
 import {MuiThemeProvider} from '@material-ui/core/styles';
 
 export const addProviders = async (ctx, muiService) => {
-  const {jss, sheetsRegistry, theme} = await muiService.from(ctx);
+  const {jss, sheetsRegistry, theme, generateClassName} = await muiService.from(
+    ctx
+  );
 
   return (
-    <JssProvider jss={jss} registry={sheetsRegistry}>
+    <JssProvider
+      jss={jss}
+      registry={sheetsRegistry}
+      generateClassName={generateClassName}
+    >
       <MuiThemeProvider theme={theme} sheetsManager={new Map()}>
         {ctx.element}
       </MuiThemeProvider>
     </JssProvider>
   );
-}
+};
