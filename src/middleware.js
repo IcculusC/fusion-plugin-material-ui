957fcfb65c05f51c584af57a8e6c7857d0f65b5c
Kevin Grandon <kevingrandon@yahoo.com>
2018-09-05 15:36:25 -0700

    Update default jss usage

--- src/middleware.js
+++ src/middleware.js
@@ -4,18 +4,27 @@ import React from 'react';
 import JssProvider from 'react-jss/lib/JssProvider';
 import {MuiThemeProvider} from '@material-ui/core/styles';
 
-export const addProviders = async (ctx, muiService) => {
-  const {jss, sheetsRegistry, theme, generateClassName} = await muiService.from(
-    ctx
-  );
+import type {Context} from 'fusion-core';
+import type {MaterialUIServiceType} from './types.js';
 
+export const addProviders = async (
+  ctx: Context,
+  muiService: MaterialUIServiceType
+) => {
+  const {
+    jss,
+    sheetsRegistry,
+    theme,
+    generateClassName,
+    sheetsManager,
+  } = muiService.from(ctx);
   return (
     <JssProvider
       jss={jss}
       registry={sheetsRegistry}
       generateClassName={generateClassName}
     >
-      <MuiThemeProvider theme={theme} sheetsManager={new Map()}>
+      <MuiThemeProvider theme={theme} sheetsManager={sheetsManager}>
         {ctx.element}
       </MuiThemeProvider>
     </JssProvider>
